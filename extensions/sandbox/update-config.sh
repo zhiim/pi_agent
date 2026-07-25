34670cfce5cfdc63e27aea2ab21110ad2832b53b
Q Xu <xuq3196@outlook.com>
2026-07-25 14:42:44 +0800

    sandbox: simplify update-config script

--- extensions/sandbox/update-config.sh
+++ extensions/sandbox/update-config.sh
@@ -6,12 +6,8 @@ AGENT_VOLUME="pi-agent-home"
 CTX_VOLUME="pi-context-mode"
 IMAGE="pi-agent-sandbox"
 
-docker volume create "$AGENT_VOLUME" >/dev/null
-docker volume create "$CTX_VOLUME" >/dev/null
-
 docker run --rm -i \
   -v "$AGENT_VOLUME:/home/piuser/.pi/agent" \
-  -v "$CTX_VOLUME:/home/piuser/.pi/context-mode" \
   --entrypoint /bin/bash \
   "$IMAGE" \
   -c 'rm -f ~/.pi/agent/.seeded'
